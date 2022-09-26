b005434223d8c55aded5fca3865fbb871703a0b9
saintajax <saintajax@gmail.com>
2022-09-26 13:17:36 +0300

    promise fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,13 +2,12 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   form: document.querySelector('.form'),
-  submit: document.querySelector('[type="submit"]'),
 };
 
-refs.submit.addEventListener('submit', onSubmit);
+refs.form.addEventListener('submit', onSubmit);
 
-function onSubmit(event) {
-  event.preventDefault();
+function onSubmit(evt) {
+  evt.preventDefault();
 
   const formData = new FormData(refs.form);
   delay = Number(formData.get('delay'));
@@ -16,13 +15,7 @@ function onSubmit(event) {
   amount = Number(formData.get('amount'));
   let deleyStep = delay;
   for (let i = 1; i <= amount; i += 1) {
-    createPromise(i, deleyStep)
-      .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
+    createPromise(i, deleyStep).then(onSuccess).catch(onError);
     deleyStep += step;
   }
 }
@@ -38,4 +31,12 @@ function createPromise(position, delay) {
       }
     }, delay);
   });
+}
+
+function onSuccess({ position, delay }) {
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function onError({ position, delay }) {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 }
